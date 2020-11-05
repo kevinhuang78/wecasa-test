486f3859898db37b6d88121f6d9ed288cd9358be
kevinhuang78 <kevin_huang_iim@yahoo.com>
2020-11-05 18:53:59 +0100

    init: react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import NotFoundExceptionScreen from "screens/Exceptions/NotFoundExceptionScreen";
+import HomeScreen from "screens/Home/HomeScreen";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+      <Switch>
+          <Route exact path="/" component={HomeScreen} />
+
+          {/* Last Route is 404 Not Found, if it doesn't find any of these route, show 404 */}
+          <Route component={NotFoundExceptionScreen} />
+      </Switch>
+  </BrowserRouter>
+)
 
 export default App;
---
+++ src/screens/Exceptions/NotFoundExceptionScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NotFoundExceptionScreen = () => <div>404 - Not found</div>;
+
+export default NotFoundExceptionScreen;
