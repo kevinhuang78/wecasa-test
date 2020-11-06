649b8a0a2b886a5db63817c9c9739b121e0ec44f
kevinhuang78 <kevin_huang_iim@yahoo.com>
2020-11-06 14:29:27 +0100

    style: add Wecasa fonts

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { WecasaHeader, WecasaContent } from './Layout.styled'
 import Logo from 'assets/Logo.png'
+import GlobalStyle from 'style/GlobalStyle'
 
 const Layout = (props) => (
   <div>
+    <GlobalStyle />
     <WecasaHeader>
       <img className="logo" src={Logo} alt="Logo Wecasa" />
     </WecasaHeader>
---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  h1, h2, h3, div { font-family: "Capriola", sans-serif; }
+  p, u, strong, i, b { font-family: "Josefin Sans", sans-serif; }
+`
+
+export default GlobalStyle
